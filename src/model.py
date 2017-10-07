741b4fc63b25706530472f7d953f029fe08764a7
Pandian <pandian4mail@gmail.com>
2017-10-07 11:13:47 -0500

    Adding keras_resnet50 model for yearbook

--- src/model.py
+++ src/model.py
@@ -7,10 +7,12 @@ from densenet121 import densenet121_model
 from densenet161 import densenet161_model
 from resnet_152 import resnet152_model
 from resnet_50 import resnet50_model
+from keras_resnet50 import keras_resnet50_model
 from util import *
 from vgg16 import vgg16_model
 from vgg19 import vgg19_model
 import customlayers
+
 
 class YearbookModel:
     get_model_function = {}
@@ -21,6 +23,7 @@ class YearbookModel:
         self.get_model_function[VGG19_ARCHITECTURE] = self.getVGG19
         self.get_model_function[RESNET152_ARCHITECTURE] = self.getResNet152
         self.get_model_function[RESNET50_ARCHITECTURE] = self.getResNet50
+        self.get_model_function[KERAS_RESNET50_ARCHITECTURE] = self.getKerasResNet50
         self.get_model_function[DENSENET169_ARCHITECTURE] = self.getDenseNet169
         self.get_model_function[DENSENET161_ARCHITECTURE] = self.getDenseNet161
         self.get_model_function[DENSENET121_ARCHITECTURE] = self.getDenseNet121
@@ -125,12 +128,14 @@ class YearbookModel:
             if model_save_path is None:
                 raise Exception('Unable to load trained model as model_save_path is None!')
             print(get_time_string() + 'Loading saved model weights from ' + model_save_path + '..')
-            model = alexnet_model(img_rows=img_rows, img_cols=img_cols, channels=channels, num_classes=NUM_CLASSES_YEARBOOK,
+            model = alexnet_model(img_rows=img_rows, img_cols=img_cols, channels=channels,
+                                  num_classes=NUM_CLASSES_YEARBOOK,
                                   use_pretraining=use_pretraining, pretrained_weights_path=pretrained_weights_path,
                                   optimizer=optimizer, loss=loss, fine_tuning_method=fine_tuning_method,
                                   weights_path=model_save_path)
         else:
-            model = alexnet_model(img_rows=img_rows, img_cols=img_cols, channels=channels, num_classes=NUM_CLASSES_YEARBOOK,
+            model = alexnet_model(img_rows=img_rows, img_cols=img_cols, channels=channels,
+                                  num_classes=NUM_CLASSES_YEARBOOK,
                                   use_pretraining=use_pretraining, pretrained_weights_path=pretrained_weights_path,
                                   optimizer=optimizer, loss=loss, fine_tuning_method=fine_tuning_method)
 
@@ -210,7 +215,8 @@ class YearbookModel:
             print(get_time_string() + 'Loading saved model from ' + model_save_path + '..')
             model = load_model(model_save_path)
         else:
-            model = vgg16_model(img_rows=img_rows, img_cols=img_cols, channels=channels, num_classes=NUM_CLASSES_YEARBOOK,
+            model = vgg16_model(img_rows=img_rows, img_cols=img_cols, channels=channels,
+                                num_classes=NUM_CLASSES_YEARBOOK,
                                 use_pretraining=use_pretraining, pretrained_weights_path=pretrained_weights_path,
                                 optimizer=optimizer, loss=loss, fine_tuning_method=fine_tuning_method)
 
@@ -296,7 +302,8 @@ class YearbookModel:
             print(get_time_string() + 'Loading saved model from ' + model_save_path + '..')
             model = load_model(model_save_path)
         else:
-            model = vgg19_model(img_rows=img_rows, img_cols=img_cols, channels=channels, num_classes=NUM_CLASSES_YEARBOOK,
+            model = vgg19_model(img_rows=img_rows, img_cols=img_cols, channels=channels,
+                                num_classes=NUM_CLASSES_YEARBOOK,
                                 use_pretraining=use_pretraining, pretrained_weights_path=pretrained_weights_path,
                                 optimizer=optimizer, loss=loss, fine_tuning_method=fine_tuning_method)
 
@@ -368,7 +375,8 @@ class YearbookModel:
             print(get_time_string() + 'Loading saved model from ' + model_save_path + '..')
             model = load_model(model_save_path, custom_objects={'Scale': customlayers.Scale})
         else:
-            model = resnet152_model(img_rows, img_cols, channels, NUM_CLASSES_YEARBOOK, use_pretraining, pretrained_weights_path,
+            model = resnet152_model(img_rows, img_cols, channels, NUM_CLASSES_YEARBOOK, use_pretraining,
+                                    pretrained_weights_path,
                                     fine_tuning_method, optimizer, loss)
 
         if initial_epoch >= num_epochs:
@@ -437,7 +445,8 @@ class YearbookModel:
             print(get_time_string() + 'Loading saved model from ' + model_save_path + '..')
             model = load_model(model_save_path)
         else:
-            model = resnet50_model(img_rows, img_cols, channels, NUM_CLASSES_YEARBOOK, use_pretraining, pretrained_weights_path,
+            model = resnet50_model(img_rows, img_cols, channels, NUM_CLASSES_YEARBOOK, use_pretraining,
+                                   pretrained_weights_path,
                                    fine_tuning_method, optimizer, loss)
 
         if initial_epoch >= num_epochs:
@@ -469,6 +478,77 @@ class YearbookModel:
             if (e % 5 == 0):
                 print(get_time_string() + 'Epoch ' + str(e) + ' complete. Evaluating on validation set..')
                 evaluateYearbookFromModel(model=model, architecture=RESNET50_ARCHITECTURE, sample=sample)
+
+            print_line()
+
+        print(get_time_string() + 'Fitting complete. Returning model..')
+
+        if model_save_path is not None:
+            print(get_time_string() + 'Saving final model to ' + model_save_path + '..')
+            model.save(model_save_path)
+
+        return model
+
+    def getKerasResNet50(self, train_images, train_labels, load_saved_model,
+                         model_save_path, use_pretraining, pretrained_weights_path, train_dir,
+                         val_dir, fine_tuning_method, batch_size, num_epochs, optimizer, loss, initial_epoch, sample):
+        """
+
+        :param load_saved_model: boolean (whether to just load the model from weights path)
+        :param model_save_path: (final model weights path, if load_pretrained is true)
+        :param pretrained_weights_path: if load_trained is false and if use_pretraining is true, the path of weights to load for pre-training
+        :param train_dir: training data directory
+        :param val_dir: validation data directory
+        :param use_pretraining: boolean, whether to use pre-training or train from scratch
+        :param fine_tuning_method: whether to use end-to-end pre-training or phase-by-phase pre-training
+        :param initial_epoch: starting epoch to start training
+        :return: Returns the AlexNet model according to the parameters provided
+
+        """
+        print(get_time_string() + 'Creating Keras ResNet50 model..')
+
+        img_rows, img_cols = 224, 224  # Resolution of inputs
+        channels = 3
+
+        if load_saved_model:
+            if model_save_path is None:
+                raise Exception('Unable to load trained model as model_save_path is None!')
+            print(get_time_string() + 'Loading saved model from ' + model_save_path + '..')
+            model = load_model(model_save_path)
+        else:
+            model = keras_resnet50_model(img_rows=img_rows, img_cols=img_cols, channels=channels,
+                                         num_classes=NUM_CLASSES_YEARBOOK, fine_tuning_method=fine_tuning_method,
+                                         optimizer=optimizer, loss=loss)
+
+        if initial_epoch >= num_epochs:
+            print(get_time_string() + 'Not fitting the model since initial_epoch is >= num_epochs. Returning model..')
+            return model
+
+        # Start Fine-tuning
+        print(get_time_string() + 'Fitting the model..')
+        for e in range(initial_epoch, num_epochs):
+            print_line()
+            print('Starting epoch ' + str(e))
+            print_line()
+            completed = 0
+
+            for x_chunk, y_chunk in chunks(train_images, train_labels, batch_size, RESNET50_ARCHITECTURE):
+                print(get_time_string() + 'Fitting model for chunk of size ' + str(len(x_chunk)) + '...')
+                model.fit(x_chunk, y_chunk,
+                          batch_size=batch_size,
+                          nb_epoch=1,
+                          verbose=1
+                          )
+                completed += len(x_chunk)
+                print(get_time_string() + str(completed) + ' of ' + str(len(train_images)) + ' complete. ')
+
+            file_name = self.getCheckpointFileName(base_model_save_path=model_save_path, epoch=e)
+            print(get_time_string() + 'Saving model to ' + file_name)
+            model.save(file_name)
+
+            if e % 3 == 0:
+                print(get_time_string() + 'Epoch ' + str(e) + ' complete. Evaluating on validation set..')
+                evaluateYearbookFromModel(model=model, architecture=KERAS_RESNET50_ARCHITECTURE, sample=sample)
 
             print_line()
 
